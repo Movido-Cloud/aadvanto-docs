4ef53d1b9807f61332b92ad56f87d2cb341a9a8f
JunaidMovido <jh@movido-media.com>
2024-05-28 13:26:19 +0200

    added translation support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,50 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-const isProduction = process.env.NODE_ENV == 'production'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+const isProduction = process.env.NODE_ENV == "production";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://movido-cloud.github.io/aadvanto-docs',
-	base: isProduction ? '/aadvanto-docs' : '/', // Conditionally set the base path
-	integrations: [
-		starlight({
-			title: 'Billing System Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					autogenerate : { directory: 'introduction' }
-				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	]
+  site: "https://movido-cloud.github.io/aadvanto-docs",
+  base: isProduction ? "/aadvanto-docs" : "/", // Conditionally set the base path
+  prefixDefaultLocale: true,
+  integrations: [
+    starlight({
+      title: "Billing System Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      // Set English as the default language for this site.
+      defaultLocale: "en",
+      locales: {
+        // English docs in `src/content/docs/en/`
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        // German docs in `src/content/docs/de/`
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
+      },
+
+      sidebar: [
+        {
+          label: "Introduction",
+          autogenerate: { directory: "introduction" },
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
